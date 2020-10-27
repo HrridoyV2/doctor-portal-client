6a6a0254052435040704f80929fe128e899690d5
unknown <hrridoyv2@gmail.com>
2020-10-27 17:39:00 +0600

    dibana

---
+++ src/components/Login/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDexfB_se7WPT2nJYpXzxFSX8YEwLSNFXY",
+  authDomain: "doctors-portal-a3f87.firebaseapp.com",
+  databaseURL: "https://doctors-portal-a3f87.firebaseio.com",
+  projectId: "doctors-portal-a3f87",
+  storageBucket: "doctors-portal-a3f87.appspot.com",
+  messagingSenderId: "74184285699",
+  appId: "1:74184285699:web:9f34474d18ebb148333836",
+};
+  export default firebaseConfig;
